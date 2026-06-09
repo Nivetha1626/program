58a619b261d996c6b369425410caf328a8fe4c2d
Nivetha <nivethadharman06@gmail.com>
2026-06-09 15:38:15 +0530

    main .java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-void main(){
-    /* program 1
+void main() {
+    /*  Marks  program 1
     // int[] Marks =new int[5];
 int[][] marks = new int[5][3];
  int[][] studentMarks= {
@@ -23,7 +23,10 @@ int[][] marks = new int[5][3];
      }
      }new int[5][3];
      */
-//program 2
+
+
+// Attendance program 2
+
 /*int[][] Attendance =new int[5][3];
     int[][] studentAttendance= {
             {0, 1,0},
@@ -49,6 +52,57 @@ int[][] marks = new int[5][3];
         System.out.println(" ");
             }
         }*/
+
+
+// calculation program 3
+    //Addtion
+   /*int result = add(5,4);
+    System.out.println(result);
+    System.out.println(add(5, 5));
+}
+public int add(int a, int b){
+    return a+b;
+}/*
+   /*
+
+   int result = sub(5,4);
+
+
+    System.out.println(result);
+    System.out.println(sub(5, 5));
+}
+public int sub(int a, int b){
+    return a-b;
+    }*/
+
+  /* multiple
+    int result = mul(5,4);
+   System.out.println(result);
+
+    System.out.println(mul(5, 5));
+        }
+public int mul(int a, int b) {
+    return a * b;
+}*/
+   /*division
+    int result = div(5,4);
+    System.out.println(result);
+
+    System.out.println(div(5, 5));
+}
+public int div(int a, int b) {
+    return a/b;
+}*/
+    /*modulus
+    int result = mol(5,4);
+    System.out.println(result);
+
+    System.out.println(mol(5, 5));
+}
+public int mol(int a, int b) {
+    return a%b;
+}*/
+
 
 
 
