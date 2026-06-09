b07ea8bff168f512cb5666293e737488b9f2a42d
Nivetha <nivethadharman06@gmail.com>
2026-06-09 14:09:28 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+void main(){
+    /* program 1
+    // int[] Marks =new int[5];
+int[][] marks = new int[5][3];
+ int[][] studentMarks= {
+    {78,98,34},
+         {89,65,89},
+    {12,34,56},
+         {66,12,89},
+         {33,77,88},
+ };
+ for(int row=0; row< studentMarks.length;row++){
+    // {78,98,34},
+    // {89,65,89},
+     //{12,34,56},
+    // {66,12,89},
+    // {33,77,88},
+     for(int col=0;col<studentMarks[row].length;col++){
+         System.out.print(studentMarks[row] [col]+" ");
+
+     }
+      System.out.println();
+     }
+     }new int[5][3];
+     */
+//program 2
+/*int[][] Attendance =new int[5][3];
+    int[][] studentAttendance= {
+            {0, 1,0},
+            {1,0,0},
+            {1,1,1},
+            {1,0,1},
+            {0,0,0},
+    };
+    for( int row=0;row <studentAttendance.length;row++){
+        System.out.println("Student"+(1+row)+":");
+        for(int column =0;column<studentAttendance[row].length;column++){
+            if(studentAttendance[row][column]==1) {                  '
+            hh
+
+
+            '
+                System.out.println("Class" + (1 + column) + ":Present");
+            }
+            else {
+                System.out.println("Class" +(1+column) + ":Absent");
+            }
+            }
+        System.out.println(" ");
+            }
+        }*/
+
+
+
+
